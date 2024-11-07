e2ee9ee1d20df96d84f02c7cb7d9ddfba3023efe
Brian Petersen <skiwalk3r@gmail.com>
2024-11-06 21:47:53 -0700

    Implements a basic version of xxd in rust

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io::{self, BufReader, Read};
+
+use itertools::Itertools;
+
+const CHUNK_SIZE: usize = 16;
+const GROUP_SIZE: usize = 2;
+
+const LINE_WIDTH: usize = 8;
+
+fn main() {
+    let reader = BufReader::new(io::stdin());
+
+    let mut line: usize = 0;
+
+    for line_chunk in &reader.bytes().chunks(CHUNK_SIZE) {
+        print!("{:0>width$x}: ", line, width = LINE_WIDTH);
+        line += CHUNK_SIZE;
+
+        for group_chunk in &line_chunk.filter_map(|x| x.ok()).chunks(GROUP_SIZE) {
+            for byte in group_chunk {
+                // Each byte is printed as a two character hex
+                print!("{:0>width$x}", byte, width = 2);
+            }
+            print!(" ");
+        }
+        println!();
+    }
+}
